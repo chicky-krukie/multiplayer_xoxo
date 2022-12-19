04821c4c261ef551e180f46bd99ab4aee3d1fd85
neko-li <erica.flores0307@gmail.com>
2022-12-20 03:05:20 +0800

    added buttons for main menu

---
+++ lib/screens/main_menu_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:multiplayer_xoxo/widget/custom_button.dart';
+
+class MainMenuScreen extends StatelessWidget {
+  const MainMenuScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CustomButton(onTap: () {}, text: 'Create Room'),
+        const SizedBox(height: 20),
+        CustomButton(onTap: () {}, text: 'Join Room'),
+      ],
+    ));
+  }
+}
