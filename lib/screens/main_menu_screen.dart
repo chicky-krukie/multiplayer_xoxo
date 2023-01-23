6e25ebb8a574deb39af34e277d34be3415b71998
justinefer <justinefer@airmail.cc>
2023-01-23 23:26:11 +0800

    make button size responsive

--- lib/screens/main_menu_screen.dart
+++ lib/screens/main_menu_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:multiplayer_xoxo/widget/custom_button.dart';
+import 'package:multiplayer_xoxo/responsive/responsive.dart';
 
 class MainMenuScreen extends StatelessWidget {
   const MainMenuScreen({super.key});
@@ -7,13 +8,16 @@ class MainMenuScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        CustomButton(onTap: () {}, text: 'Create Room'),
-        const SizedBox(height: 20),
-        CustomButton(onTap: () {}, text: 'Join Room'),
-      ],
-    ));
+      body: Responsive(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomButton(onTap: () {}, text: 'Create Room'),
+            const SizedBox(height: 20),
+            CustomButton(onTap: () {}, text: 'Join Room'),
+          ],
+        )
+      )
+    );
   }
 }
