60d52dec92f37a0417bde022cfe509ea4207d85d
justinefer <justinefer@airmail.cc>
2023-01-25 12:54:59 +0800

    room creation

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:multiplayer_xoxo/screens/create_room_screen.dart';
 import 'package:multiplayer_xoxo/screens/join_room_screen.dart';
 import 'package:multiplayer_xoxo/screens/main_menu_screen.dart';
+import 'package:multiplayer_xoxo/screens/game_screen.dart';
 import 'package:multiplayer_xoxo/utils/colors.dart';
 
 void main() {
@@ -22,6 +23,7 @@ class MyApp extends StatelessWidget {
         MainMenuScreen.routeName: (context) => const MainMenuScreen(),
         CreateRoomScreen.routeName: (context) => const CreateRoomScreen(),
         JoinRoomScreen.routeName: (context) => const JoinRoomScreen(),
+        GameScreen.routeName: (context) => const GameScreen(),
       },
       initialRoute: MainMenuScreen.routeName,
     );
--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ require('dotenv').config()
 const app = express();
 const port = process.env.PORT | 3000;
 var server = http.createServer(app);
+const Room = require('./models/room');
 
 var io = require('socket.io')(server);
 
@@ -16,8 +17,28 @@ const database = process.env.DB_URL;
 
 io.on('connection', (socket) => {
     console.log('Socket.io connection established');
-    socket.on('createRoom', ({ nickname }) => {
+    socket.on('createRoom', async ({ nickname }) => {
         console.log(nickname);
+        try {
+            let room = new Room();
+            let player = {
+                socketID: socket.id,
+                nickname: nickname,
+                playerType: 'X',
+            };
+            room.players.push(player);
+            room.turn = player;
+            room = await room.save();
+
+            console.log(room);
+            const roomID = room._id.toString();
+            socket.join(roomID);
+
+            io.to(roomID).emit('createRoomSuccess', room);
+
+        } catch (e) {
+            console.log(e);
+        }
     });
 });
 
